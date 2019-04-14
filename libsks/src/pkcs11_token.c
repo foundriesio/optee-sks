35e5ec0199cd39265f26f3532af472703004b969
Ricardo Salveti <ricardo@foundries.io>
2019-04-13 21:09:45 -0300

    [toup] libsks: fix invalid mechanism reporting in mech info
    Undefined mechanism should be reported as invalid instead of device
    error.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- libsks/src/pkcs11_token.c
+++ libsks/src/pkcs11_token.c
@@ -260,8 +260,8 @@ CK_RV sks_ck_token_mechanism_info(CK_SLOT_ID slot,
 	ctrl[0] = (uint32_t)slot;
 	ctrl[1] = ck2sks_mechanism_type(type);
 	if (ctrl[1] == SKS_UNDEFINED_ID) {
-		LOG_ERROR("mechanism is not support by this library\n");
-		return CKR_DEVICE_ERROR;
+		LOG_DEBUG("mechanism is not support by this library\n");
+		return CKR_MECHANISM_INVALID;
 	}
 
 	/* info is large enought, for sure */
