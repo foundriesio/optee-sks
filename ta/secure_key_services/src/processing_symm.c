f94aeb4967bdf0e7247d6dc0c5417cfae4071bc3
Ricardo Salveti <ricardo@foundries.io>
2019-04-29 21:34:33 -0300

    [toup] sks ta: symm: validate input buffer
    Make sure input buffer is valid for operations that requires it.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- ta/secure_key_services/src/processing_symm.c
+++ ta/secure_key_services/src/processing_symm.c
@@ -459,6 +459,11 @@ uint32_t step_symm_operation(struct pkcs11_session *session,
 		    step == SKS_FUNC_STEP_ONESHOT)
 			break;
 
+		if (!in) {
+			DMSG("No input data");
+			return SKS_BAD_PARAM;
+		}
+
 		switch (function) {
 		case SKS_FUNCTION_ENCRYPT:
 		case SKS_FUNCTION_DECRYPT:
@@ -478,6 +483,11 @@ uint32_t step_symm_operation(struct pkcs11_session *session,
 	case SKS_CKM_AES_GCM:
 		if (step == SKS_FUNC_STEP_FINAL)
 			break;
+
+		if (!in) {
+			DMSG("No input data");
+			return SKS_BAD_PARAM;
+		}
 
 		switch (function) {
 		case SKS_FUNCTION_ENCRYPT:
@@ -546,6 +556,10 @@ uint32_t step_symm_operation(struct pkcs11_session *session,
 	case SKS_CKM_AES_CBC_PAD:
 	case SKS_CKM_AES_CTS:
 	case SKS_CKM_AES_CTR:
+		if (step == SKS_FUNC_STEP_ONESHOT && !in) {
+			DMSG("No input data");
+			return SKS_BAD_PARAM;
+		}
 		switch (function) {
 		case SKS_FUNCTION_ENCRYPT:
 		case SKS_FUNCTION_DECRYPT:
